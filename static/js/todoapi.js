f3ce5233576ab507f2637f6a3ae178506af88c21
Suhaas Korampally <korampallysuhaas@gmail.com>
2021-05-14 21:41:58 +0530

    Aded css for TODO

--- static/js/todoapi.js
+++ static/js/todoapi.js
@@ -8,7 +8,7 @@ function displayTodos()
             result = JSON.parse( result );
             for ( let key = result.length - 1; key >= 0; key-- )
             {
-                document.getElementById( "todos" ).innerHTML += `\n<div class="todo-item" id="div-${result[key].todoId}" ><label id="lbl-${result[key].todoId}" onclick="editTask(this)">${result[key].task}</label><button id="${result[key].todoId}" class="btn" onclick="deleteTask(this)">remove</button></div>\n`
+                document.getElementById( "todos" ).innerHTML += `\n<div class="todo-item" id="div-${result[key].todoId}" ><label id="lbl-${result[key].todoId}" onclick="editTask(this)">${result[key].task}</label><button id="${result[key].todoId}" class="btn" onclick="deleteTask(this)">done</button></div>\n`
             }
         },
         error: function ( error )
@@ -32,7 +32,7 @@ function addTask()
         data: data
     } );
     ele = document.getElementById( "todos" );
-    ele.innerHTML = `\n<div class="todo-item" id="div-${data.todoId}" ><label id="lbl-${data.todoId}" onclick="editTask(this)">${data.task}</label><button id="${data.todoId}" class="btn" onclick="deleteTask(this)">remove</button></div>\n` + ele.innerHTML;
+    ele.innerHTML = `\n<div class="todo-item" id="div-${data.todoId}" ><label id="lbl-${data.todoId}" onclick="editTask(this)">${data.task}</label><button id="${data.todoId}" class="btn" onclick="deleteTask(this)">done</button></div>\n` + ele.innerHTML;
     document.getElementById( "input" ).value = "";
 }
 
@@ -65,5 +65,17 @@ function editTask( obj )
                 data: data
             } );
         }
+    } );
+    $( `#${id}` ).focusout( function ( event )
+    {
+        document.getElementById( id ).setAttribute( "contentEditable", "false" );
+        var task = document.getElementById( id ).innerHTML;
+        todoId = id.slice( 4 );
+        var data = {task: task, todoId: todoId};
+        $.ajax( {
+            url: `http://${window.location.host}/api/todos/${todoId}`,
+            type: 'PUT',
+            data: data
+        } );
     } );
 }
